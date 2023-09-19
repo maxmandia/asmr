c7af08f54e1c1f8408dcfb204df12c7b4790a4f2
MaxMandia <mmandia511@gmail.com>
2023-09-18 22:25:04 -0400

    feat: added clerk components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { useUser } from "@clerk/nextjs";
 import Layout from "~/components/Layout";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { UserButton, SignInButton } from "@clerk/nextjs";
 export default function Home() {
   const user = useUser();
+  console.log(user);
 
   if (!user.isLoaded) {
     return <div>Loading...</div>;
@@ -11,8 +13,13 @@ export default function Home() {
   return (
     <div className="flex items-center justify-between px-5 py-7">
       <div>[ASMR]</div>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center gap-2">
         <MagnifyingGlassIcon height={25} width={25} color="white" />
+        {user.isSignedIn ? (
+          <UserButton afterSignOutUrl="/sign-in" />
+        ) : (
+          <SignInButton afterSignInUrl="/" />
+        )}
       </div>
     </div>
   );
