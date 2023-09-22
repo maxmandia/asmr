1a39d00ceb2765eace277852914e341ec6c309e3
MaxMandia <mmandia511@gmail.com>
2023-09-22 17:09:11 -0400

    feat: added file uploading

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,20 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "~/components/Layout";
 import UserProfileImage from "~/components/UserProfileImage";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import { ImageIcon } from "@radix-ui/react-icons";
+import { useUploadThing } from "~/lib/utils/uploadthing";
+
 export default function Page() {
+  const [file, setFile] = useState<File[] | null>(null);
+
+  const {
+    startUpload,
+    permittedFileInfo = {
+      config: {
+        video: {
+          maxFileCount: 1,
+          maxFileSize: "4GB",
+        },
+      },
+    },
+  } = useUploadThing("imageUploader", {
+    onClientUploadComplete: () => {
+      alert("uploaded successfully!");
+    },
+    onUploadError: () => {
+      alert("error occurred while uploading");
+    },
+    onUploadBegin: () => {
+      alert("upload has begun");
+    },
+  });
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+    const userFile = event.target.files[0];
+    if (userFile) {
+      setFile([userFile]);
+    }
+  };
+
+  const openFileDialog = () => {
+    const fileInput = document.getElementById("fileInput") as HTMLInputElement;
+    if (fileInput) {
+      fileInput.click();
+    }
+  };
+
   return (
     <div className="flex flex-col px-5 py-7">
       <div className="flex items-start justify-between ">
         <div className="flex items-center gap-3">
           <UserProfileImage />
-          <button className="bg-input flex items-center gap-1 rounded-[6px] px-[10px] py-[5px]">
+          <button className="flex items-center gap-1 rounded-[6px] bg-input px-[10px] py-[5px]">
             <span className="text-[14px] font-medium">subscribers only</span>
             <ChevronDownIcon height={20} width={20} color="white" />
           </button>
         </div>
-        <button className="bg-primary rounded-[6px] px-[20px] py-[10px] text-[14px] font-medium">
+        <button
+          onClick={() => {
+            if (file) {
+              startUpload(file);
+            }
+          }}
+          className="rounded-[6px] bg-primary px-[20px] py-[10px] text-[14px] font-medium"
+        >
           post!
         </button>
       </div>
@@ -24,8 +72,17 @@ export default function Page() {
           placeholder="Share something :)"
         />
       </div>
-      <div className="bg-input my-4 h-[1px] w-full" />
-      <button className="bg-input flex w-fit items-center gap-1 rounded-[6px] px-[15px] py-[5px]">
+      <div className="my-4 h-[1px] w-full bg-input" />
+      <button
+        onClick={openFileDialog}
+        className="flex w-fit items-center gap-1 rounded-[6px] bg-input px-[15px] py-[5px]"
+      >
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: "none" }}
+          onChange={handleFileChange}
+        />
         <ImageIcon height={20} width={20} color="white" />
         <span className="text-medium">upload media</span>
       </button>
