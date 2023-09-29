526359b9f800f5ae9ef38cac1fdc9e5f39f295f4
MaxMandia <mmandia511@gmail.com>
2023-09-28 22:03:27 -0400

    fix: build

--- src/components/UserPost.tsx
+++ src/components/UserPost.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Post } from "@prisma/client";
 import Image from "next/image";
 function UserPost(data: Post) {
-  return (
-    <div>
-      <Image src={data.image} />
-    </div>
-  );
+  return <div>{/* <Image src={data.image} /> */}</div>;
 }
 
 export default UserPost;
