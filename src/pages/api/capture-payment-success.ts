6a4857a9a2ed0f27664aecadfe0e20d47f95c4c4
Max <mmandia511@gmail.com>
2023-11-13 17:40:26 -0500

    feat: debugger json file

--- src/pages/api/capture-payment-success.ts
+++ src/pages/api/capture-payment-success.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
-Stripe.PaymentIntentsResource;
 export default async function handler(
   req: NextApiRequest,
   resp: NextApiResponse,
 ) {
-  console.log("got ya");
+  console.log(req);
   return resp.status(200).json({ status: "ok" });
 }
