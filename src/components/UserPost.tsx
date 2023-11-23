95ad34b6744f210245a79bc1f1482fbcf1059750
Max <mmandia511@gmail.com>
2023-11-23 17:46:34 -0500

    fix: poster issue on mobile

--- src/components/UserPost.tsx
+++ src/components/UserPost.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import {
   EnterFullScreenIcon,
@@ -102,6 +102,26 @@ function PostVideo(post: Post) {
       }
     }
   }
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const hiddenVideo = document.createElement("video");
+      hiddenVideo.crossOrigin = "anonymous";
+      hiddenVideo.src = post.video ?? "";
+      hiddenVideo.currentTime = 0;
+      hiddenVideo.onseeked = () => {
+        const canvas = document.createElement("canvas");
+        canvas.width = hiddenVideo.videoWidth;
+        canvas.height = hiddenVideo.videoHeight;
+        const ctx = canvas.getContext("2d");
+        ctx?.drawImage(hiddenVideo, 0, 0, canvas.width, canvas.height);
+        const posterUrl = canvas.toDataURL();
+        if (videoRef.current) {
+          videoRef.current.poster = posterUrl;
+        }
+      };
+    }
+  }, []);
 
   if (!post.video) return null;
 
