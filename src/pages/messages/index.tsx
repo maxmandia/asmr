ead5b38de96dba6c50fecca196abe50585014d52
Max <mmandia511@gmail.com>
2023-11-24 18:00:50 -0500

    style: styling

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,5 +1,5 @@
-import { Cross1Icon } from "@radix-ui/react-icons";
-import React, { useState } from "react";
+import { ArrowLeftIcon, Cross1Icon } from "@radix-ui/react-icons";
+import React, { useRef, useState } from "react";
 import Layout from "~/components/Layout";
 import { api } from "~/lib/utils/api";
 import Image from "next/image";
@@ -7,17 +7,27 @@ import debounce from "lodash.debounce";
 import { User } from "~/types/User";
 
 function Messages() {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [showNewMessageModal, setShowNewMessageModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  async function sendHandler() {
+    //
+  }
+
   return (
-    <div className="h-full w-full p-5 md:flex md:items-center md:justify-between">
+    <div className="fixed h-[92%] w-full p-5 md:flex md:items-center md:justify-between md:gap-5 ">
       {showNewMessageModal && (
         <NewMessageModal
           setSelectedUser={setSelectedUser}
           setShowNewMessageModal={setShowNewMessageModal}
         />
       )}
-      <div className="h-full md:w-2/3 lg:w-1/3">
+      <div
+        className={`h-full md:w-2/3 lg:w-[50%] ${
+          selectedUser && "hidden md:block"
+        }`}
+      >
         <div className="flex items-center justify-between">
           <span>Messages</span>
           <button onClick={() => setShowNewMessageModal(true)}>New</button>
@@ -27,9 +37,42 @@ function Messages() {
           {/* <span className="text-grey">No messages yet...</span> */}
         </div>
       </div>
-      <div className="hidden md:block">
-        <p className="text-grey">Message contents will show up here...</p>
-      </div>
+      {/* <p className="text-grey">Message contents will show up here...</p> */}
+      {selectedUser && (
+        <>
+          <div className="flex items-center gap-3">
+            <button className="md:hidden">
+              <ArrowLeftIcon height={24} width={24} />
+            </button>
+            <div className="flex items-center gap-2">
+              {selectedUser.profile_picture_url ? (
+                <div className="relative h-[40px] w-[40px] rounded-[100px]">
+                  <Image
+                    src={selectedUser.profile_picture_url}
+                    alt={`${selectedUser.first_name}'s profile picture`}
+                    layout="fill"
+                    objectFit="cover"
+                    priority={true}
+                    className="rounded-[100px]"
+                  />
+                </div>
+              ) : (
+                <div className="h-[40px] w-[40px] rounded-[40px] bg-primary" />
+              )}
+              <span className="text-[20px] font-semibold">
+                {selectedUser.first_name}
+              </span>
+            </div>
+          </div>
+          {/* <div className="relative flex h-[88%] items-center justify-center bg-red-200"> */}
+          <input
+            ref={inputRef}
+            className="absolute bottom-5 left-0 right-0 mx-3 rounded-[6px] bg-input py-2 pl-2 focus:outline-none"
+            type="text"
+            placeholder="Say hello (indoor voice please)"
+          />
+        </>
+      )}
     </div>
   );
 }
@@ -57,7 +100,7 @@ function NewMessageModal({
 
   return (
     <div className="absolute bottom-0 left-0 right-0 top-0 z-[100] flex items-center justify-center bg-black bg-opacity-50">
-      <div className="fixed h-screen w-screen bg-background p-5 md:h-fit md:w-3/12 md:rounded-[6px] md:p-10">
+      <div className="h-full w-screen bg-background p-5 md:h-fit md:w-3/12 md:rounded-[6px] md:p-10">
         <div className="flex w-full items-center gap-5">
           <button onClick={() => setShowNewMessageModal(false)}>
             <Cross1Icon height={18} width={18} />
@@ -110,12 +153,12 @@ function UserMessageCard({
       }}
       className="w-full"
     >
-      <div className="flex items-center gap-5 rounded-[6px] py-2 pl-1 hover:cursor-pointer hover:bg-card_hover">
+      <div className="flex items-center gap-2 rounded-[6px] py-2 pl-1 hover:cursor-pointer hover:bg-card_hover">
         {user.profile_picture_url ? (
           <div className="relative h-[45px] w-[45px] rounded-[100px] bg-red-100">
             <Image
               src={user.profile_picture_url}
-              alt={`${user.first_name}'s profile header`}
+              alt={`${user.first_name}'s profile picture`}
               layout="fill"
               objectFit="cover"
               priority={true}
