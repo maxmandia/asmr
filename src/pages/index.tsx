6b695cba761de1588e15e7c818ba1288a5fd1aee
Max <mmandia511@gmail.com>
2023-12-13 18:29:16 -0500

    feat: started mobile landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,132 @@
-import React, { useRef } from "react";
+import React, { useMemo, useState } from "react";
 import { api } from "~/lib/utils/api";
 import toast from "react-hot-toast";
+import Link from "next/link";
+import Image from "next/image";
+
+const bongoURL = "/sounds/bongo.m4a";
+const clocURL = "/sounds/cloc.m4a";
+const malletURL = "/sounds/mallet.m4a";
 
 function Landing() {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const { mutate } = api.waitlists.addUserToWaitlist.useMutation({
-    onSuccess: () => {
-      toast.success("added to waitlist");
-    },
-    onError: () => {
-      toast.error("error occurred adding to waitlist");
-    },
-  });
+  const [isClicked, setIsClicked] = useState<Record<string, boolean> | null>(
+    null,
+  );
+
+  const boxShadowStyle = {
+    boxShadow:
+      "0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+  };
+
+  const bubbleTextStyle = {
+    color: "transparent",
+    WebkitTextStroke: ".5px white",
+  };
+
+  const bubbleTextStyleWithShadow = {
+    ...bubbleTextStyle,
+    ...boxShadowStyle,
+  };
+
+  function handleKeyPress(key: string) {
+    toggleAudio(key);
+    setIsClicked({ ...isClicked, [key]: true });
+    setTimeout(() => {
+      setIsClicked((prev) => ({ ...prev, [key]: false }));
+    }, 100);
+  }
+
+  function toggleAudio(key: string) {
+    switch (key) {
+      case "ctrl":
+        const audio = new Audio(bongoURL);
+        audio.play();
+        break;
+      case "cmd":
+        const audio2 = new Audio(clocURL);
+        audio2.play();
+        break;
+      case "alt":
+        const audio3 = new Audio(malletURL);
+        audio3.play();
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div>
-      <header className="flex h-screen flex-col items-center justify-center gap-3 px-2">
-        <h1 className="text-[33px] font-semibold md:text-[50px] lg:text-[65px]">
-          Welcome to Hush ASMR
-        </h1>{" "}
-        <p className="w-full px-1 text-center text-[15px] text-zinc-400 md:max-w-[600px] md:text-[20px] lg:max-w-[700px] lg:text-[25px]">
-          A subscription platform empowering ASMR creators to own their full
-          potential, monetize their content, and develop authentic connections
-          with their fans.
-        </p>
-        <div className="flex w-full items-center justify-between gap-2 px-6 py-2 md:max-w-[500px]">
-          <input
-            ref={emailRef}
-            type="email"
-            placeholder="creator@email.com"
-            aria-label="Email for creators"
-            className="w-full rounded-md px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-primary" // Focus styles added for better UX
-          />
+      <div className="flex flex-col items-center justify-center gap-1 py-[200px]">
+        <span className="mr-20 -rotate-[34deg] self-end text-[30px]">🦋</span>
+        <h1 className="font-sf text-[50px] font-bold leading-none">
+          Hush Asmr
+        </h1>
+        <span className="w-[250px] text-center font-sf text-[20px] text-[#676767]">
+          Where exceptional sounds get discovered.
+        </span>
+        <div className="my-5 flex items-end gap-[5px] rounded-[11px] bg-[#C9CBD2] p-[5px] text-[16px]">
           <button
-            onClick={() => {
-              mutate({
-                email: emailRef.current?.value ?? "",
-              });
-            }}
-            className="rounded-md bg-primary p-2 text-white"
+            onClick={() => handleKeyPress("ctrl")}
+            style={
+              isClicked && isClicked.ctrl
+                ? bubbleTextStyle
+                : bubbleTextStyleWithShadow
+            }
+            className="rounded-[6px] border-[1px] border-solid border-black bg-input px-[10px] py-[8px]"
           >
-            Waitlist
-          </button>{" "}
+            ctrl
+          </button>
+          <button
+            onClick={() => handleKeyPress("cmd")}
+            style={
+              isClicked && isClicked.cmd
+                ? bubbleTextStyle
+                : bubbleTextStyleWithShadow
+            }
+            className="rounded-[6px] border-[1px] border-solid border-black bg-input px-[10px] py-[8px]"
+          >
+            cmd
+          </button>
+          <Link
+            href="/home"
+            onClick={() => handleKeyPress("watch")}
+            style={isClicked && isClicked.watch ? {} : boxShadowStyle}
+            className="rounded-[6px] border-[1px] border-solid border-black border-opacity-20 bg-primary px-[20px] py-[8px] font-sf"
+          >
+            start watching
+          </Link>
+          <button
+            onClick={() => handleKeyPress("alt")}
+            style={
+              isClicked && isClicked.alt
+                ? bubbleTextStyle
+                : bubbleTextStyleWithShadow
+            }
+            className="rounded-[6px] border-[1px] border-solid border-black bg-input px-[10px] py-[8px]"
+          >
+            alt
+          </button>
         </div>
-      </header>
-      <main>
-        <section className="flex flex-col items-center justify-center gap-3 bg-gray-800 px-2 py-8 text-center">
-          <h2 className="text-[26px] font-semibold lg:text-[40px]">
-            ASMR is inherently sexy.
-          </h2>{" "}
-          <p className="w-full px-1 text-center text-[15px] text-zinc-400 lg:max-w-[700px] lg:text-[20px]">
-            ASMR should be playful, sensual, and sexy. We believe that ASMR can
-            be a powerful tool for intimacy and connection. Experience the power
-            of sexy ASMR.
-          </p>
-        </section>
-        <section className="flex flex-col items-center justify-center gap-3 bg-gray-700 px-2 py-8 text-center">
-          <h2 className="text-[26px] font-semibold lg:text-[40px]">
-            Empowering content creators.
-          </h2>
-          <p className="w-full px-1 text-center text-[15px] text-zinc-400 lg:max-w-[700px] lg:text-[20px]">
-            Creators should fully own their potential, monetize their content
-            effectively, and build genuine connections with their audience.
-            We&apos;re here to make that happen.
-          </p>
-        </section>
-        <section className="flex flex-col items-center justify-center gap-3 bg-gray-800 px-2 py-8 text-center">
-          <h2 className="text-[26px] font-semibold lg:text-[40px]">
-            A platform tailored for ASMR.
-          </h2>
-          <p className="w-full px-1 text-center text-[15px] text-zinc-400 lg:max-w-[700px] lg:text-[20px]">
-            From dark mode to high-quality audio and a focus on community,
-            we&apos;re crafting an experience that meets the unique needs of the
-            ASMR community.
-          </p>
-        </section>
-      </main>
-      <footer className="py-4 text-center text-white">
-        <p>© {new Date().getFullYear()} Hush ASMR. All rights reserved.</p>
-      </footer>
+        <div className="absolute bottom-[-100px] left-0 right-0 overflow-hidden">
+          <div className="relative h-[300px] w-[300px]">
+            <Image
+              src={"/images/cloud.png"}
+              className="ml-[-20px]"
+              alt="a beautiful cloud"
+              layout="fill"
+              objectFit="contain"
+            />
+            <Image
+              src={"/images/cloud.png"}
+              className="ml-[175px]"
+              alt="a beautiful cloud"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
