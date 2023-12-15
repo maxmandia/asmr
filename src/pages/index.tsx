0a647975c32a16a0a4fc5a160555aac139c00842
Max <mmandia511@gmail.com>
2023-12-15 08:53:30 -0500

    style: cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,33 @@ import { api } from "~/lib/utils/api";
 import toast from "react-hot-toast";
 import Link from "next/link";
 import Image from "next/image";
+import { CookieIcon, MagicWandIcon } from "@radix-ui/react-icons";
+import styles from "~/styles/home.module.css";
 
 const bongoURL = "/sounds/bongo.m4a";
 const clocURL = "/sounds/cloc.m4a";
 const malletURL = "/sounds/mallet.m4a";
+
+const animals = [
+  "bunny",
+  "cow",
+  "dog",
+  "fox",
+  "monkey",
+  "owl",
+  "panda",
+  "pig",
+  "tiger",
+];
+
+const peopleColors = [
+  "blue-person",
+  "green-person",
+  "orange-person",
+  "purple-person",
+  "red-person",
+  "yellow-person",
+];
 
 function Landing() {
   const [isClicked, setIsClicked] = useState<Record<string, boolean> | null>(
@@ -57,14 +80,14 @@ function Landing() {
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center gap-1 py-[200px]">
-        <span className="mr-20 -rotate-[34deg] self-end text-[30px] md:ml-[500px] md:self-center md:text-[50px]">
+      <div className="flex h-screen flex-col items-center gap-1 py-40">
+        <span className="mr-20 -rotate-[34deg] self-end text-[30px] md:ml-[500px] md:self-center md:text-[40px] lg:text-[50px]">
           🦋
         </span>
         <h1 className="font-sf text-[50px] font-bold leading-none md:text-[80px] lg:text-[90px]">
           Hush Asmr
         </h1>
-        <span className="w-[250px] text-center font-sf text-[20px] text-[#676767] md:w-full md:text-[24px]">
+        <span className="w-[250px] text-center font-sf text-[20px] text-[#797979] md:w-full md:text-[24px]">
           Where exceptional sounds get discovered.
         </span>
         <div className="my-5 flex items-end gap-[5px] rounded-[11px] bg-[#C9CBD2] p-[5px] text-[16px]">
@@ -111,7 +134,7 @@ function Landing() {
           </button>
         </div>
         <div className="absolute bottom-[-100px] left-0 right-0 ml-[-20px] flex items-center justify-between overflow-hidden md:bottom-[-150px]">
-          <div className="relative h-[300px] w-[300px] flex-shrink-0 md:h-[400px] md:w-[400px]">
+          <div className="relative h-[300px] w-[300px] flex-shrink-0 md:h-[400px] md:w-[400px] lg:opacity-50">
             <Image
               src={"/images/cloud.png"}
               className=""
@@ -120,7 +143,7 @@ function Landing() {
               objectFit="contain"
             />
           </div>
-          <div className="relative z-10 ml-[-150px] h-[300px] w-[300px] flex-shrink-0 md:h-[400px] md:w-[400px]">
+          <div className="relative z-10 ml-[-150px] h-[300px] w-[300px] flex-shrink-0 md:h-[400px] md:w-[400px] lg:opacity-70">
             <Image
               src={"/images/cloud.png"}
               className=""
@@ -129,7 +152,7 @@ function Landing() {
               objectFit="contain"
             />
           </div>
-          <div className="relative z-10 ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block">
+          <div className="relative z-[0] ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block lg:opacity-80">
             <Image
               src={"/images/cloud.png"}
               className=""
@@ -138,7 +161,7 @@ function Landing() {
               objectFit="contain"
             />
           </div>
-          <div className="relative z-10 ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block">
+          <div className="relative z-10 ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block lg:opacity-80">
             <Image
               src={"/images/cloud.png"}
               className=""
@@ -147,7 +170,7 @@ function Landing() {
               objectFit="contain"
             />
           </div>
-          <div className="relative z-10 ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block">
+          <div className="relative z-[0] ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block lg:opacity-80">
             <Image
               src={"/images/cloud.png"}
               className=""
@@ -156,7 +179,7 @@ function Landing() {
               objectFit="contain"
             />
           </div>
-          <div className="relative z-10 ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block">
+          <div className="relative z-10 ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block lg:opacity-70">
             <Image
               src={"/images/cloud.png"}
               className=""
@@ -165,7 +188,7 @@ function Landing() {
               objectFit="contain"
             />
           </div>
-          <div className="relative z-10 ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block">
+          <div className="relative z-[0] ml-[-150px] hidden h-[400px] w-[400px] flex-shrink-0 md:block lg:opacity-50">
             <Image
               src={"/images/cloud.png"}
               className=""
@@ -174,6 +197,74 @@ function Landing() {
               objectFit="contain"
             />
           </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-10 px-4 py-[200px]">
+        <div
+          style={boxShadowStyle}
+          className={`${styles.gradient_border_2} relative flex  flex-col items-center justify-center rounded-[50px] border-[2px] border-solid border-transparent bg-input p-5`}
+        >
+          <div className="flex w-[98%] flex-col gap-3">
+            <MagicWandIcon height={50} width={50} />
+            <h2 className="font-sf text-[28px] font-bold leading-[33px]">
+              Chat with creators like you would with friends.
+            </h2>
+            <p className="text-[18px] leading-[22px] text-[#797979]">
+              Send tips, photos, videos and more for an intimate experience
+              that’s truly magical.
+            </p>
+          </div>
+          <div className="flex items-center gap-1 overflow-scroll pt-6">
+            {animals.map((animal) => (
+              <div className="flex-shrink-0" key={animal}>
+                <Image
+                  className="select-none"
+                  src={`/images/${animal}.png`}
+                  alt="animal"
+                  height={80}
+                  width={80}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div
+          style={boxShadowStyle}
+          className={`${styles.gradient_border} relative flex  flex-col items-center justify-center rounded-[50px] border-[2px] border-solid border-transparent bg-input p-5`}
+        >
+          <div className="flex w-[98%] flex-col gap-3">
+            <CookieIcon height={50} width={50} />
+            <h2 className="font-sf text-[28px] font-bold leading-[33px]">
+              Relaxing, fun, sexy asmr - here for you 24/7.{" "}
+            </h2>
+            <p className="text-[18px] leading-[22px] text-[#797979]">
+              Access exclusive content from your favorite asmr creators that can
+              only be found here.
+            </p>
+          </div>
+          <div className="flex items-center gap-1 pt-6">
+            {peopleColors.map((person) => (
+              <div className="flex-shrink-0" key={person}>
+                <Image
+                  className="select-none"
+                  src={`/images/person-icons/${person}.png`}
+                  alt="person"
+                  height={50}
+                  width={50}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex w-full flex-col items-center">
+          <div className="ml-10 flex w-full flex-col">
+            <span className="font-sf text-[50px] font-bold">Hush.</span>
+            <span className="text-[18px] text-[#797979]">
+              Did you hear that ad?
+            </span>
+            <span className="text-[18px] text-[#797979]">Neither did we.</span>
+          </div>
+          <span className="text-[100px]">🤫</span>
         </div>
       </div>
     </div>
