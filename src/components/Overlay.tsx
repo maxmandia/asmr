bfa20946357af7b751b909993b553e118824df00
Max <mmandia511@gmail.com>
2024-02-25 14:07:55 -0500

    feat: using react portal for tip modal

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ClientOnlyPortal from "./ClientOnlyPortal";
 
 function Overlay({ children }: { children: React.ReactNode }) {
   return (
-    <div className="absolute bottom-0 left-0 right-0 top-0 z-[100] flex items-center justify-center bg-black bg-opacity-50">
-      {children}
-    </div>
+    <ClientOnlyPortal selector="#overlay">
+      <div className="absolute bottom-0 left-0 right-0 top-0 z-[100] flex items-center justify-center bg-black bg-opacity-50">
+        {children}
+      </div>
+    </ClientOnlyPortal>
   );
 }
 
